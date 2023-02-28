ca3791d8241d01489183ebc9a01acf11f9d238f0
Juan Ibarra <jmibarra86@gmail.com>
2023-02-28 20:32:40 -0300

    Se termina la version inicial de la interfaz de usuario y pruebo de generar los css desde 0

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,31 @@ function App() {
             <div className="side-menu-button"> <span>+</span>New chat</div>
         </aside>
         <section className="chatbox">
-
+            <div className="chat-log">
+                <div className="chat-message">
+                    <div className="chat-message-center">
+                        <div className="avatar">
+                        
+                        </div>
+                        <div className="message">
+                            Hola mundo
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-message chatgpt">
+                    <div className="chat-message-center">
+                        <div className="avatar">
+                        
+                        </div>
+                        <div className="message">
+                            Hola mundo de respuesta
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="chat-input-holder">
+                <textarea rows="1" className="chat-input-textarea" placeholder="Type your message here"></textarea>
+            </div>
         </section>
     </div>
   );
