c0633a659ed7e00437a1304c10de5a329261d15e
Juan Ibarra <jmibarra86@gmail.com>
2023-02-28 20:03:31 -0300

    Se sube el proyecto inicializado y se comienza a trabajar en la barra izquierda del chat

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <aside className="sidemenu">
+            <div className="side-menu-button"> <span>+</span>New chat</div>
+        </aside>
+        <section className="chatbox">
+
+        </section>
+    </div>
+  );
+}
+
+export default App;
