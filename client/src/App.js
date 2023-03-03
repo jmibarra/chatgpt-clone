5c050a37fc89d1e5976b52fc334e25ccf0b362e3
Juan Ibarra <jmibarra86@gmail.com>
2023-03-03 19:50:58 -0300

    Saco todo a un componente

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,8 @@
 import './App.css';
-import {useState} from 'react'
-import ChatMessage from './Components/ChatMessage';
+import ChatLayout from './Components/ChatLayout';
 function App() {
-
-    const [input, setInput] = useState("")
-    const [chatLog, setChatLog] = useState([{
-        user: "gpt",
-        message: "¿Cómo puedo ayudarte?"
-
-    }])
-
-    async function handleSubmit(e){
-        e.preventDefault();
-        setChatLog([...chatLog,{ user:"me", message: `${input}`}])
-        setInput("");
-
-
-    
-    }
   return (
-    <div className="App">
-        <aside className="sidemenu">
-            <div className="side-menu-button"> <span>+</span>New chat</div>
-        </aside>
-        <section className="chatbox">
-            <div className="chat-log">
-                {chatLog.map( (message,index) => (
-                    <ChatMessage key={index} message={message}/>
-                ))}
-            </div>
-            <div className="chat-input-holder">
-                <form onSubmit={handleSubmit}> 
-                    <input 
-                        rows="1" 
-                        value={input}
-                        onChange={ (e) => setInput(e.target.value) }
-                        className="chat-input-textarea" 
-                        placeholder="Type your message here"
-                        ></input>
-                </form>
-            </div>
-        </section>
-    </div>
+    <ChatLayout/>
   );
 }
 
