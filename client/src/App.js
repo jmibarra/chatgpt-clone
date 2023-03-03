eaaacbb72d9dfaac1b22ed4bd8ff46f81f6fcce7
Juan Ibarra <jmibarra86@gmail.com>
2023-03-03 19:47:09 -0300

    Armo la privera version funcionando del componente de chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+
+    const [input, setInput] = useState("")
+    const [chatLog, setChatLog] = useState([{
+        user: "gpt",
+        message: "¿Cómo puedo ayudarte?"
+
+    }])
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setChatLog([...chatLog,{ user:"me", message: `${input}`}])
+        setInput("");
+
+
+    
+    }
   return (
     <div className="App">
         <aside className="sidemenu">
@@ -8,32 +24,38 @@ function App() {
         </aside>
         <section className="chatbox">
             <div className="chat-log">
-                <div className="chat-message">
-                    <div className="chat-message-center">
-                        <div className="avatar">
-                        </div>
-                        <div className="message">
-                            Hola mundo
-                        </div>
-                    </div>
-                </div>
-                <div className="chat-message chatgpt">
-                    <div className="chat-message-center">
-                        <div className="avatar">
-                        
-                        </div>
-                        <div className="message">
-                            Hola mundo de respuesta
-                        </div>
-                    </div>
-                </div>
+                {chatLog.map( (message,index) => (
+                    <ChatMessage key={index} message={message}/>
+                ))}
             </div>
             <div className="chat-input-holder">
-                <textarea rows="1" className="chat-input-textarea" placeholder="Type your message here"></textarea>
+                <form onSubmit={handleSubmit}> 
+                    <input 
+                        rows="1" 
+                        value={input}
+                        onChange={ (e) => setInput(e.target.value) }
+                        className="chat-input-textarea" 
+                        placeholder="Type your message here"
+                        ></input>
+                </form>
             </div>
         </section>
     </div>
   );
+}
+
+const ChatMessage = ({ message }) => {
+    return (
+        <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
+            <div className="chat-message-center">
+                <div className="avatar">
+                </div>
+                <div className="message">
+                    {message.message}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
