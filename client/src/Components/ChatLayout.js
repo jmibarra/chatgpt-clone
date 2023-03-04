ec2c0af87800a344415edd479a3ed5effd03071f
Juan Ibarra <jmibarra86@gmail.com>
2023-03-03 23:07:12 -0300

    Ajustes de estilos

--- client/src/Components/ChatLayout.js
+++ client/src/Components/ChatLayout.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ChatMessage from './ChatMessage';
 
 const ChatLayout = () => {
@@ -8,9 +8,9 @@ const ChatLayout = () => {
         message: "¿Cómo puedo ayudarte?"
     }])
 
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault();
-        setChatLog([...chatLog,{ user:"me", message: `${input}`}])
+        setChatLog([...chatLog, { user: "me", message: `${input}` }])
         setInput("");
 
         const response = await fetch("http://localhost:3080/", {
@@ -19,39 +19,39 @@ const ChatLayout = () => {
                 "Content-type": "application/json"
             },
             body: JSON.stringify({
-                message: chatLog.map( (message) => message.message).join("")
+                message: chatLog.map((message) => message.message).join("")
             })
         })
 
         const data = await response.json();
-        console.log(data),
+        console.log(data)
     }
 
-  return (
-    <div className="App">
-        <aside className="sidemenu">
-            <div className="side-menu-button"> <span>+</span>New chat</div>
-        </aside>
-        <section className="chatbox">
-            <div className="chat-log">
-                {chatLog.map( (message,index) => (
-                    <ChatMessage key={index} message={message}/>
-                ))}
-            </div>
-            <div className="chat-input-holder">
-                <form onSubmit={handleSubmit}> 
-                    <input 
-                        rows="1" 
-                        value={input}
-                        onChange={ (e) => setInput(e.target.value) }
-                        className="chat-input-textarea" 
-                        placeholder="Type your message here"
+    return (
+        <div className="App">
+            <aside className="sidemenu">
+                <div className="side-menu-button"> <span>+</span>New chat</div>
+            </aside>
+            <section className="chatbox">
+                <div className="chat-log">
+                    {chatLog.map((message, index) => (
+                        <ChatMessage key={index} message={message} />
+                    ))}
+                </div>
+                <div className="chat-input-holder">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            rows="1"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            className="chat-input-textarea"
+                            placeholder="Type your message here"
                         ></input>
-                </form>
-            </div>
-        </section>
-    </div>
-  );
+                    </form>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default ChatLayout
