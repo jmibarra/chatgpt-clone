e3b5aafbfd8355c91a337e1f3552511073120073
Juan Ibarra <jmibarra86@gmail.com>
2023-03-03 22:40:56 -0300

    Ajustes visuales de los iconos

--- client/src/Components/ChatMessage.js
+++ client/src/Components/ChatMessage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { ReactComponent as ChatGPTAvatar } from '../images/chatgpt-icon.svg'
+import { ReactComponent as UserAvatar } from '../images/default-profile-picture-male-icon.svg'
 
 const ChatMessage = ({message}) => {
   return (
     <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
         <div className="chat-message-center">
             <div className="avatar">
+              {message.user === "gpt" ? <ChatGPTAvatar/> : <UserAvatar/>}
             </div>
             <div className="message">
                 {message.message}
